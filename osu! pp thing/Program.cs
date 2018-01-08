190a82f5e8de887e241b45ede6c471733b0c34ff
HoLLy <dario.db@hotmail.com>
2018-01-08 16:09:25 +0100

    Add project files.

---
+++ osu! pp thing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osu__pp_thing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
