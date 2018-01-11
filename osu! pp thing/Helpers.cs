112969fb915879c32ba7438bca27a19ad81c030b
HoLLy <dario.db@hotmail.com>
2018-01-11 17:39:56 +0100

    Minor improvements to PP display

--- osu! pp thing/Helpers.cs
+++ osu! pp thing/Helpers.cs
@@ -56,8 +56,12 @@ namespace osu_pp_tools
                 void ShowWithPercent(double percent)
                 {
                     var acc = new Accuracy(percent, map.Objects.Count, 0);
-                    var pp = new PPv2(new PPv2Parameters { Beatmap = map, AimStars = diff.Aim, SpeedStars = diff.Speed, Mods = mods, Count100 = acc.Count100, Count50 = acc.Count50 });
-                    Console.Write($" | {(pp.Total.ToString("F2") + "pp").PadRight(9)} {("(" + percent.ToString(CultureInfo.CurrentCulture) + "%)").PadLeft(4 + 3)}");
+                    var pp = new PPv2(new PPv2Parameters {
+                        Beatmap = map, Mods = mods,
+                        AimStars = diff.Aim, SpeedStars = diff.Speed,
+                        Count300 = acc.Count300, Count100 = acc.Count100, Count50 = acc.Count50, CountMiss = acc.CountMiss
+                    });
+                    Console.Write($" | {$"{pp.Total:F2}pp".PadRight(9)} ({percent.ToString(CultureInfo.InvariantCulture)}%)");
                 }
 
                 Console.Write($"{modStr.PadRight(6)}| {diff.Total:F2}* (Aim: {diff.Aim:F2}*, Speed: {diff.Speed:F2}*)");
